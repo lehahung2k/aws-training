ea8671b1dae4fee3aa898ebcfb5d0438a383e7a9
Le Ha Hung <hung_lh@flinters.vn>
2024-11-29 17:09:21 +0700

    fix error subnet

--- environments/test/ec2.tf
+++ environments/test/ec2.tf
@@ -3,9 +3,10 @@ module "server_test" {
   project_name = var.project_name
   environment  = var.environment
   vpc_id       = data.terraform_remote_state.main_vpc.outputs.vpc_id
-  # Subnet
+  # Network
   ec2_subnet_cidr_block = var.ec2_subnet_cidr_block
   availability_zone     = var.availability_zone
+  internet_gateway_id   = data.terraform_remote_state.main_vpc.outputs.internet_gateway_id
 
   # Security
   allowed_ssh_ips         = var.allowed_ssh_ips
