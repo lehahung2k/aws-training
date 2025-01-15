9fb671eda6a51901e08f0a281757748428594436
Le Ha Hung <hung2000yt@gmail.com>
2025-01-15 23:49:27 +0700

    feat: add ansible and CICD pipeline

--- terraform/environments/test/provider.tf
+++ terraform/environments/test/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket  = "hunglh-terraform-remote-state-20240711"
+    key     = "environment/test/terraform.tfstate"
+    region  = "ap-southeast-1"
+    encrypt = true
+  }
 }
 
 provider "aws" {
